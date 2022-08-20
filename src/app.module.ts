c8ac1a7feb14867d842041f37ce7049a4e18d107
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-20 20:20:28 -0300

    chore: conect to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
 
+config();
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [AppController],

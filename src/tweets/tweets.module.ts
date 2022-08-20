ebcf1d9ac5c1b58144dec1ba54a5169e701e7fd2
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-20 20:58:09 -0300

    feat: update tweets module

--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tweet, TweetSchema } from './tweet';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Tweet.name,
+        schema: TweetSchema,
+      },
+    ]),
+  ],
+  exports: [],
+  controllers: [],
+  providers: [],
+})
 export class TweetsModule {}

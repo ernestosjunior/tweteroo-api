60c34def049b5ffdc2d778ac622f07863c5350de
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-20 20:54:04 -0300

    feat: create tweets module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
 import { UsersModule } from './users/users.module';
+import { TweetsModule } from './tweets/tweets.module';
 
 config();
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), UsersModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
+    TweetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

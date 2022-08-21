298a1f6fd99ee79c57593e2673ce695c15d74217
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-20 21:11:18 -0300

    feat: remove unused methods on user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,31 +1,13 @@
-import { Controller, Get, Param, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { User } from './user';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('sign-up')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  async getAll(): Promise<User[]> {
-    return this.usersService.listAllUsers();
-  }
-
-  @Get(':id')
-  async getById(@Param('id') id: string): Promise<User> {
-    return this.usersService.getUserById(id);
-  }
-
-  @Put(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() userUpdated: User,
-  ): Promise<User> {
-    return this.usersService.updateUser(id, userUpdated);
-  }
-
-  @Delete(':id')
-  async delete(@Param('id') id: string): Promise<User> {
-    return this.usersService.deleteUser(id);
+  @Post()
+  async create(@Body() user: User): Promise<User> {
+    return this.usersService.create(user);
   }
 }

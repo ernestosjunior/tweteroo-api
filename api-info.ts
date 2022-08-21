f0851811c7e07904c4ed97a0bf885004181d1307
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-20 21:15:23 -0300

    feat: update app controller

---
+++ api-info.ts
@@ -0,0 +1,8 @@
+export default {
+  name: 'tweteroo-api',
+  version: '0.0.1',
+  description: '',
+  author: '',
+  private: true,
+  license: 'UNLICENSED',
+};
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import apiInfo from '../api-info';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return apiInfo;
   }
 }
